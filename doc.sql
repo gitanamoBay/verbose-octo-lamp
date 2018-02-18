081f3e41b6e00d50ca05e6575134be973dc0d0d8
grant <grant.lodge@outlook.com>
2018-02-18 15:40:37 +0000

    sc

--- doc.sql
+++ doc.sql
@@ -3,15 +3,15 @@ CREATE TABLE changes (
   date TIMESTAMP NOT NULL,
   userId INT NOT NULL,
   data json NOT NULL
-)
+);
 
 CREATE TABLE users(
   id SERIAL PRIMARY KEY,
   username VARCHAR(64) NOT NULL,
   password VARCHAR(256) NOT NULL,
   enabled BOOLEAN NOT NULL DEFAULT (TRUE)
- )
+ );
 
-INSERT INTO users (username, password) VALUES ('grant', 'pass')
+INSERT INTO users (username, password) VALUES ('grant', 'pass');
 
-SELECT * from users WHERE username = 'grant'
+SELECT * from users WHERE username = 'grant';
