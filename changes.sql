678d6b0d92efc7d106baefc4634d6094a247cd9a
grant lodge <grant.lodge@outlook.com>
2018-02-18 22:08:54 +0000

    data

--- changes.sql
+++ changes.sql
@@ -1,13 +1,16 @@
---DROP TABLE changes;
+DROP TABLE changes;
 
 CREATE TABLE changes (
   id SERIAL PRIMARY KEY,
-  date TIMESTAMP NOT NULL,
-  userId INT NOT NULL,
+  date_logged TIMESTAMP NOT NULL DEFAULT (current_timestamp),
+  user_id INT NOT NULL,
+  status SMALLINT NOT NULL DEFAULT (0),
   impact INT NOT NULL,
-  data json NOT NULL
+  data jsonb NOT NULL,
+  date_started TIMESTAMP NULL,
+  date_finished TIMESTAMP NULL
 );
 
-INSERT INTO changes (date, userid, impact, data) SELECT current_date, 1, 1, '{ "test" :1 }';
+INSERT INTO changes (user_id, impact, data) SELECT 1, 1, '{ "test" :1 }';
 
 SELECT * FROM changes;
